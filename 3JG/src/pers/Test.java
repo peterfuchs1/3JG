f41a2c58ed3e12ae3ef1279a48d54c8bf84ce9a1
peterfuchs1 <peterfuchs_1@yahoo.de>
2016-06-06 09:13:20 +0200

    simple TreeSet: use a readable variable

--- 3JG/src/pers/Test.java
+++ 3JG/src/pers/Test.java
@@ -5,13 +5,13 @@ import java.util.TreeSet;
 public class Test {
 
 	public static void main(String[] args) {
-		TreeSet<Person> tm=new TreeSet<>();
-		tm.add(new Person("Wolfgang","Berger"));
-		tm.add(new Person("Hans","Weber"));
-		tm.add(new Person("Peter","Müller"));
-		tm.add(new Person("Renate","Bauer"));
-		tm.add(new Person("Patrizia","Franz"));
-		for(Person p:tm)
+		TreeSet<Person> persons=new TreeSet<>();
+		persons.add(new Person("Wolfgang","Berger"));
+		persons.add(new Person("Hans","Weber"));
+		persons.add(new Person("Peter","Müller"));
+		persons.add(new Person("Renate","Bauer"));
+		persons.add(new Person("Patrizia","Franz"));
+		for(Person p:persons)
 			System.out.println(p.toString());
 	}
 
